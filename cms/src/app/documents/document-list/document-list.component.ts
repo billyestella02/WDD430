2f8d747052f66b8ba6e8fb08707719bb12b035e4
Billy Estella <billyestella02@gmail.com>
2022-05-12 20:35:30 +0800

    added week04 assignment

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    {
+      id: '1',
+      name: 'doc1',
+      description: 'description1',
+      url: 'url1.com',
+      children: null
+    },
+    {
+      id: '2',
+      name: 'doc2',
+      description: 'description2',
+      url: 'url2.com',
+      children: null
+    },
+    {
+      id: '3',
+      name: 'doc3',
+      description: 'description3',
+      url: 'url3.com',
+      children: null
+    },
+    {
+      id: '4',
+      name: 'doc4',
+      description: 'description4',
+      url: 'url4.com',
+      children: null
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
